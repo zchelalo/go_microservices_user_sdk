2b278417a23bf8eb4ec347f15e38be2b55710899
zchelalo <eduardosaavedra687@gmail.com>
2024-07-31 22:21:08 -0700

    update: improve error message

--- user/transport.go
+++ user/transport.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	"errors"
 	"fmt"
 	"net/url"
 	"time"
@@ -56,7 +55,7 @@ func (c *clientHTTP) Get(id string) (*domain.User, error) {
 			return nil, ErrNotFound{dataResponse.Message}
 		}
 
-		return nil, errors.New(dataResponse.Message)
+		return nil, fmt.Errorf("%s", dataResponse.Message)
 	}
 
 	return dataResponse.Data.(*domain.User), nil
